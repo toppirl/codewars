77aa0b396510c32e429e453f4f87496617c6f5db
Jordan Topp <toppdat@gmail.com>
2022-12-29 21:20:35 -0800

    Added convert a string to array

---
+++ 8kyu/convert-a-string-to-an-array.js
@@ -0,0 +1,17 @@
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// My solution
+
+function stringToArray(string) {
+  return string.split(' ');
+}
+
+// Best Practice
+
+function stringToArray(string) {
+  return string.split(' ');
+}
