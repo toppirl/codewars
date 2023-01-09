819468451bfdbfaa5919a7a1ed56abd4e48c1b58
Jordan Topp <toppdat@gmail.com>
2023-01-09 09:50:33 -0800

    Added disemvowel trolls

---
+++ 7kyu/disemvowel-trolls.js
@@ -0,0 +1,21 @@
+// Trolls are attacking your comment section!
+
+// A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+// Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+// For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+// My solution
+
+function disemvowel(str) {
+    //split string into characters to check if vowel
+    return str.split('').filter((char) => 'aeiou'.indexOf(char.toLowerCase()) === -1).join('')
+    //if vowel, push to string
+  }
+
+ // Best Practice
+
+ function disemvowel(str) {
+    return str.replace(/[aeiou]/gi, '');
+  }
