9f27fd98ce68f76c6f4cd5b0aef3fd37af1e8ab5
Jordan Topp <toppdat@gmail.com>
2023-01-09 15:36:37 -0800

    Updated highest and lowest

--- 7kyu/highest-and-lowest.js
+++ 7kyu/highest-and-lowest.js
@@ -21,12 +21,7 @@ function highAndLow(numbers){
   }
 
   // Best Practice
-
-  function highAndLow(numbers){
-    numbers = numbers.split(' ').map(Number);
-    return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
-  }
-
+  
   function highAndLow(numbers){
     numbers = numbers.split(' ');
     return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
