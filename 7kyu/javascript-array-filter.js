a0dc5d85449176004fe940e675a21bbe33e8c43d
Jordan Topp <toppdat@gmail.com>
2023-02-03 16:53:06 -0800

    Added javascript array filter

---
+++ 7kyu/javascript-array-filter.js
@@ -0,0 +1,8 @@
+// filter out the odd numbers
+
+
+// My Solution && Best Practice
+
+const getEvenNumbers = (numbersArray) => numbersArray.filter(num => num % 2 === 0)
+
+console.log(getEvenNumbers([1,2,3,6,8,10]))
