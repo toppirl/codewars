44084d7a2978d4e75a9d0d48b5ab76a838c91597
Jordan Topp <toppdat@gmail.com>
2023-02-18 16:15:04 -0800

    Updated find numbers

--- 8kyu/find-numbers-which-are-divisible-by-a-given-number.js
+++ 8kyu/find-numbers-which-are-divisible-by-a-given-number.js
@@ -4,7 +4,7 @@
 
 // [1, 2, 3, 4, 5, 6], 2 --> [2, 4, 6]
 
-// My solution and Best Practice
+// My solution and Best Practice the same
 
 function divisibleBy(numbers, divisor){
   return numbers.filter(num => num % divisor === 0)
