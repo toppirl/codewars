c37c51c97249260ae30e99eff0d240b2d1eef6d6
Jordan Topp <toppdat@gmail.com>
2023-02-20 16:20:41 -0800

    Updated to square or not

--- 8kyu/to-square-or-not-to-square.js
+++ 8kyu/to-square-or-not-to-square.js
@@ -8,3 +8,21 @@
 
 // [4,3,9,7,2,1] -> [2,9,3,49,4,1]
 
+function squareOrSquareRoot(array) {
+    return array.map(int => {
+        if(Math.sqrt(int) % 1 !== 0) {
+            return int ** 2 
+        } else {
+            return Math.sqrt(int)
+        }
+    }) 
+  }
+
+// Best Practice
+
+function squareOrSquareRoot(array) {
+    return array.map(x => {
+      const r = Math.sqrt(x);
+      return (r % 1 == 0) ? r : (x*x);
+    });  
+  }
