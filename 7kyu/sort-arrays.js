819e8a1588a2dbf93ff374d8bee7d133a7278dcb
Jordan Topp <toppdat@gmail.com>
2023-03-01 20:05:06 -0800

    Added sort arrays

---
+++ 7kyu/sort-arrays.js
@@ -0,0 +1,8 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+// My solution && Best Practice
+
+const sortMe =  names =>  names.sort()
+
+console.log(sortMe(['Jordan', 'Sandra', 'Alex']))
+
