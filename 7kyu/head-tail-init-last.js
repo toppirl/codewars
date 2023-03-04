16c115f1b7bc4416e1a23107b1132d8552a7a29c
Jordan Topp <toppdat@gmail.com>
2023-03-03 22:35:32 -0800

    Update head tail init last

--- 7kyu/head-tail-init-last.js
+++ 7kyu/head-tail-init-last.js
@@ -1,5 +1,31 @@
+// | HEAD | <----------- TAIL ------------> |
+// [  1,  2,  3,  4,  5,  6,  7,  8,  9,  10]
+// | <----------- INIT ------------> | LAST |
+
+// head [x] = x
+// tail [x] = []
+// init [x] = []
+// last [x] = x
+
+// My solution
+
 const init = (arr) => {
     return arr.slice(0,arr.length-1)
 }
 
+const head = (arr) => {
+    return arr[0]
+}
+
+const tail = arr => {
+    return arr.slice(1)
+}
+
+const last = arr => {
+    return arr[arr.length-1]
+}
+
 console.log(init(['hello', 'hi', 'how are you']))
+console.log(head([5,1]))
+console.log(tail(['hello', 'hi', 'how are you']))
+console.log(last(['hello', 'hi', 'how are you']))
