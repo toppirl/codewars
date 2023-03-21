1f6b9dbce7086a85e2b66f328ae3b1bcbec45e8b
Jordan Topp <toppdat@gmail.com>
2023-03-21 10:32:49 -0700

    Updated get the middle character

--- 7kyu/get-the-middle-character.js
+++ 7kyu/get-the-middle-character.js
@@ -22,10 +22,8 @@
 function getMiddle(s)
 {
   if(s.length % 2 === 0) {
-
+    return `${s[((s.length)/2)-1]}${s[((s.length)/2)]}`
   } else {
     return s[(s.length-1)/2]
   }
 }
-
-getMiddle('happy')
