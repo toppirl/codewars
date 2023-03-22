a845360770634fc1ff0667d9f85d8f0a97d8116e
Jordan Topp <toppdat@gmail.com>
2023-03-22 11:59:02 -0700

    Updated get the middle character

--- 7kyu/get-the-middle-character.js
+++ 7kyu/get-the-middle-character.js
@@ -27,3 +27,10 @@ function getMiddle(s)
     return s[(s.length-1)/2]
   }
 }
+
+// Best Practice
+
+function getMiddle(s)
+{
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
