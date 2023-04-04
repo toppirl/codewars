2f4be085c404a2b80877777c844dd2e89833215d
Jordan Topp <toppdat@gmail.com>
2023-04-04 12:59:06 -0700

    Added string ends with

---
+++ 7kyu/string-ends-with.js
@@ -0,0 +1,22 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+// My solution
+
+function solution(str, ending){
+    if(ending.length === 0) {
+        return true;
+    } else {
+        return  str.slice(-(ending.length)) === ending ? true : false;
+    }
+  }
+
+ // Best Practice
+
+ function solution(str, ending){
+    return str.endsWith(ending);
+  }
