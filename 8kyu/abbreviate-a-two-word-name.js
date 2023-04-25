480c433ec26f6c19e6f2601fb3564694c6cca56f
Jordan Topp <toppdat@gmail.com>
2023-04-25 10:45:26 -0700

    Updated abbreviate a two word name

--- 8kyu/abbreviate-a-two-word-name.js
+++ 8kyu/abbreviate-a-two-word-name.js
@@ -21,3 +21,7 @@ function abbrevName(name){
 function abbrevName(name){
     return name.split(' ').map(i => i[0].toUpperCase()).join('.')
 }
+
+function abbrevName(name){
+    return name.split(' ').map(x => x.substr(0, 1).toUpperCase()).join('.');
+  }
