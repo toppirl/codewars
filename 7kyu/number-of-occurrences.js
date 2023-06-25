b7d885ea1c4111b76a24ac95421e2e10edcfae66
Jordan Topp <toppdat@gmail.com>
2023-06-25 13:30:43 -0700

    Added number of occurances

---
+++ 7kyu/number-of-occurrences.js
@@ -0,0 +1,28 @@
+// Write a function that returns the number of occurrences of an element in an array.
+
+// This function will be defined as a property of Array with the help of the method Object.defineProperty, which allows to define a new method directly on the object (more info about that you can find on MDN).
+
+// Examples
+// var arr = [0, 1, 2, 2, 3];
+// arr.numberOfOccurrences(0) === 1;
+// arr.numberOfOccurrences(4) === 0;
+// arr.numberOfOccurrences(2) === 2;
+// arr.numberOfOccurrences(3) === 1;
+
+const numberOfOccurances = (e, num) =>  {
+    let count = 0;
+    for(let i = 0; i < e.length; i++) {
+        console.log(i)
+        if(e[i] === num) {
+            count += 1;
+        }
+    }
+    console.log(count)
+    return count
+}
+
+// Best Practice
+
+Array.prototype.numberOfOccurrences = function(search) {
+    return this.filter( function(num){ return search === num } ).length;
+  }
