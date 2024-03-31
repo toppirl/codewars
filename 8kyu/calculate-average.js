45d8beb15de26062845192413a8fe31f399dd0ef
Jordan Topp <toppdat@gmail.com>
2024-03-30 17:51:34 -0700

    Added calculate average

--- 8kyu/calculate-average.js
+++ 8kyu/calculate-average.js
@@ -4,12 +4,16 @@
 
 // My solution
 
-function findAverage(array) {
-    return array.length === 0 ? 0 : array.reduce((sum, val) => sum + val ) / array.length
-  }
+const testScores = [87, 76, 99, 45, 98, 'this is albert', 'and his friend', 0]
+console.log(testScores[5] + ' ', testScores[6])
+// [0, 1, 2 ,3] // these are the indexes
+
+// function findAverage(array) {
+//     return array.length === 0 ? 0 : array.reduce((sum, val) => sum + val ) / array.length
+//   }
 
 // Best Practice
 
-var find_average = (array) => {  
-    return array.length === 0 ? 0 : array.reduce((acc, ind)=> acc + ind, 0)/array.length
-  }
+// var find_average = (array) => {
+//     return array.length === 0 ? 0 : array.reduce((acc, ind)=> acc + ind, 0)/array.length
+//   }
